14ec279dabcbf5ce6e9f214f0311d5b6b0791bff
RaunakSarkar-dev <70904092+RaunakSarkar-dev@users.noreply.github.com>
2020-09-08 20:05:28 +0530

    Create MessageQueue_Basic_Impl.cpp

---
+++ MessageQueue-Basic/MessageQueue_Basic_Impl.cpp
@@ -0,0 +1,11 @@
+#include "MessageQueue_Impl.h"
+
+int main()
+{
+    Student1 s1;
+    s1.submitHomework();
+    Student2 s2;
+    s2.submitHomework();
+    Teacher t1;
+    t1.receiveHomework();
+}
